a73f9873646e48afb56a866fbff935b48e760dba
Eric Lee <macbookpro4eric@Mac-mini.local>
2024-03-22 10:04:21 +0900

    Add example case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,53 @@
 "use client";
-import Image from "next/image";
-import { useEffect } from "react";
 
-// import { Web3Exception } from "@injectivelabs/exceptions";
-// import { TxRaw } from "@injectivelabs/sdk-ts";
 import { ChainId, EthereumChainId } from "@injectivelabs/ts-types";
 import { Wallet, WalletStrategy } from "@injectivelabs/wallet-ts";
 
 export default function Home() {
-  useEffect(() => {
-    const walletStrategy = new WalletStrategy({
-      chainId: ChainId.Mainnet,
-    });
-  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Injective wallet integration
+      <div
+        onClick={() => {
+          const metamaskWallet = new WalletStrategy({
+            chainId: ChainId.Mainnet,
+            ethereumOptions: {
+              ethereumChainId: EthereumChainId.Mainnet,
+              // rpcUrl: alchemyRpcEndpoint
+            },
+          });
+          metamaskWallet.setWallet(Wallet.Metamask);
+          metamaskWallet.getAddresses().then((addresses) => {
+            console.log(addresses);
+            metamaskWallet
+              .signArbitrary(addresses[0], "hello world")
+              .then((d) => {
+                console.log(d);
+              });
+          });
+          metamaskWallet.onAccountChange((addrs) => console.log(addrs));
+        }}
+      >
+        {Wallet.Metamask}
+      </div>
+      <div
+        onClick={() => {
+          const keplrWallet = new WalletStrategy({
+            chainId: ChainId.Mainnet,
+          });
+          keplrWallet.setWallet(Wallet.Keplr);
+          console.log(keplrWallet.strategies);
+          keplrWallet.getAddresses().then((addresses) => {
+            console.log(addresses);
+            keplrWallet.signArbitrary(addresses[0], "hello world").then((d) => {
+              console.log(d);
+            });
+          });
+          keplrWallet.onAccountChange((addrs) => console.log(addrs));
+        }}
+      >
+        Keplr
+      </div>
     </main>
   );
 }
